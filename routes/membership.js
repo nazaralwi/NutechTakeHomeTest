37fef2b6bbed6d31a70ba157094850240a57c8f6
Nazar Alwi <alwinazar75@gmail.com>
2025-11-03 23:50:54 +0700

    Add email validation

--- routes/membership.js
+++ routes/membership.js
@@ -4,6 +4,11 @@ let router = express.Router();
 router.post('/registration', (req, res, next) => {
   const { email, first_name, last_name, password } = req.body;
 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ status: 102, message: 'Paramter email tidak sesuai format', data: null });
+  }
+
   res.status(201).json({ status: 0, message: 'Registrasi berhasil silahkan login', data: null });
 });
 
