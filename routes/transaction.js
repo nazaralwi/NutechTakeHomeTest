5fc53f3e47c107d1d3a44839f07ba332284b1644
Nazar Alwi <alwinazar75@gmail.com>
2025-11-05 03:21:42 +0700

    Add transaction feature

--- routes/transaction.js
+++ routes/transaction.js
@@ -126,5 +126,108 @@ router.post('/topup', async (req, res, next) => {
   }
 });
 
+router.post('/transaction', async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    const { service_code } = req.body;
+
+    if (!authHeader) {
+      return res.status(401).json({
+        status: 108,
+        message: 'Token tidak ditemukan. Harap login terlebih dahulu.',
+        data: null,
+      });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    let decoded;
+    try {
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      return res.status(401).json({
+        status: 108,
+        message: 'Token tidak tidak valid atau kadaluwarsa',
+        data: null,
+      });
+    }
+
+    const { email } = decoded;
+
+    const getUserQuery = {
+      text: 'SELECT id, balance FROM users WHERE email = $1',
+      values: [email],
+    };
+    const user = (await pool.query(getUserQuery)).rows[0];
+
+    const getServicesQuery = {
+      text: 'SELECT * FROM services',
+    }
+    const services = (await pool.query(getServicesQuery)).rows;
+
+    const service = services.find((service) => service.service_code === service_code);
+
+    if (!service) {
+      return res.status(400).json({
+        status: 102,
+        message: 'Service atau Layanan tidak ditemukan',
+        data: null,
+      });
+    }
+
+    const isBalanceSufficient = user.balance >= service.service_tariff;
+
+    if (!isBalanceSufficient) {
+      return res.status(400).json({
+        status: 102,
+        message: 'Saldo tidak cukup',
+        data: null,
+      });
+    }
+
+    const reduceBalanceQuery = {
+      text: 'UPDATE users SET balance = balance - $1 WHERE id = $2',
+      values: [service.service_tariff, user.id],
+    };
+    await pool.query(reduceBalanceQuery);
+
+    const invoiceNumber = generateInvoiceNumber();
+    const transactionDate = new Date().toISOString();
+
+    const recordTransactionQuery = {
+      text: `
+        INSERT INTO transactions 
+        (id, invoice_number, transaction_type, description, total_amount, created_on, user_id) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+      `,
+      values: [
+        `transaction-${nanoid(16)}`,
+        invoiceNumber,
+        'PAYMENT',
+        service.service_name,
+        service.service_tariff,
+        transactionDate,
+        user.id,
+      ],
+    };
+
+    await pool.query(recordTransactionQuery);
+
+    res.status(200).json({ 
+      status: 0, 
+      message: 'Transaksi berhasil', 
+      data: {
+        invoice_number: invoiceNumber,
+        service_code: service.service_code,
+        service_name: service.service_name,
+        transaction_type: 'PAYMENT',
+        total_amount: service.service_tariff,
+        created_on: transactionDate,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ status: 102, message: err.message, data: null });
+  }
+});
 
 module.exports = router;
